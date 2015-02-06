51018c5202d033ce12ca2111e135cdeda1d7d6f5
HEmile <emilevankrieken@live.nl>
2015-02-06 21:10:25 +0100

    Added equals override for WorldMinecraft

---
+++ src/main/test/com/gildedgames/util/io_manager/factory/IOFactoryTest.java
@@ -0,0 +1,16 @@
+package com.gildedgames.util.io_manager.factory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IOFactoryTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
