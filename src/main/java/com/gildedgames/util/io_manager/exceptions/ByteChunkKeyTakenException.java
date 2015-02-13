18e50316a5a89a8815904744ee9a6f4bf530f8c9
Brandon Pearce <brandonpearce@gilded-games.com>
2015-02-13 23:21:55 +0800

    Small exception output change

--- src/main/java/com/gildedgames/util/io_manager/exceptions/ByteChunkKeyTakenException.java
+++ src/main/java/com/gildedgames/util/io_manager/exceptions/ByteChunkKeyTakenException.java
@@ -8,7 +8,7 @@ public class ByteChunkKeyTakenException extends RuntimeException
 
 	public ByteChunkKeyTakenException(String key)
 	{
-		super("The key " + key + " has already been taken within a ByteChunkPool! Contact code monkeys!");
+		super("The key '" + key + "' has already been taken within a ByteChunkPool! Contact code monkeys!");
 	}
 
 }
