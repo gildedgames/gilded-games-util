4f04cdfd1f9ce0012266e927361bd0cacb81b7c6
HEmile <emilevankrieken@live.nl>
2015-02-14 22:32:40 +0100

    Fixed menu core switching

--- src/main/java/com/gildedgames/util/menu/MenuServices.java
+++ src/main/java/com/gildedgames/util/menu/MenuServices.java
@@ -3,20 +3,22 @@ package com.gildedgames.util.menu;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.client.gui.GuiScreen;
+
 import com.gildedgames.util.menu.client.IMenu;
 
 public class MenuServices
 {
 
 	protected List<IMenu> menus = new ArrayList<IMenu>();
-	
-	protected IMenu current;	
-	
+
+	protected IMenu current;
+
 	public MenuServices()
 	{
-		
+
 	}
-	
+
 	public IMenu getMenuFromID(String id)
 	{
 		for (IMenu menu : this.menus)
@@ -26,52 +28,64 @@ public class MenuServices
 				return menu;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public IMenu getNextMenu()
 	{
 		int index = this.menus.indexOf(this.getCurrentMenu()) + 1;
-		
+
 		if (index >= this.menus.size())
 		{
 			index = 0;
 		}
-		
+
 		return this.menus.get(index);
 	}
-	
+
 	public IMenu getPreviousMenu()
 	{
 		int index = this.menus.indexOf(this.getCurrentMenu()) - 1;
-		
+
 		if (index < 0)
 		{
 			index = this.menus.size() - 1;
 		}
-		
+
 		return this.menus.get(index);
 	}
-	
+
+	public IMenu fromGui(GuiScreen screen)
+	{
+		for (IMenu menu : this.menus)
+		{
+			if (screen.getClass().isAssignableFrom(menu.getMenuClass()))
+			{
+				return menu;
+			}
+		}
+		return null;
+	}
+
 	public List<IMenu> getRegisteredMenus()
 	{
 		return new ArrayList<IMenu>(this.menus);
 	}
-	
+
 	public void setCurrentMenu(IMenu menu)
 	{
 		this.current = menu;
 	}
-	
+
 	public IMenu getCurrentMenu()
 	{
 		return this.current;
 	}
-	
+
 	public void registerMenu(IMenu menu)
 	{
 		this.menus.add(menu);
 	}
-	
+
 }
--- src/main/java/com/gildedgames/util/menu/client/MenuClientEvents.java
+++ src/main/java/com/gildedgames/util/menu/client/MenuClientEvents.java
@@ -80,11 +80,16 @@ public class MenuClientEvents
 			return;
 		}
 
+		int mouseX = Mouse.getX();
+		int mouseY = Mouse.getY();
+
 		MenuCore.locate().setCurrentMenu(menu);
 
 		this.mc.displayGuiScreen(menu.getNewInstance());
 
 		menu.onOpen();
+
+		Mouse.setCursorPosition(mouseX, mouseY);
 
 		if (!shouldSaveToConfig)
 		{
@@ -110,7 +115,11 @@ public class MenuClientEvents
 	{
 		GuiScreen gui = event.gui;
 
-		if (gui instanceof GuiMainMenu && MenuCore.locate().getCurrentMenu() == null)
+		IMenu menu = MenuCore.locate().getCurrentMenu();
+
+		IMenu hooked = MenuCore.locate().fromGui(gui);
+
+		if ((gui instanceof GuiMainMenu || hooked != null) && menu == null || gui.getClass() == GuiMainMenu.class && menu.getMenuClass() != GuiMainMenu.class)
 		{
 			event.setCanceled(true);
 
@@ -133,6 +142,10 @@ public class MenuClientEvents
 			{
 				this.openMenu(ClientProxy.MINECRAFT_MENU);
 			}
+		}
+		else if (gui != null && menu != null && !gui.getClass().isAssignableFrom(menu.getMenuClass()))
+		{
+			MenuCore.locate().setCurrentMenu(null);
 		}
 	}
 
@@ -183,25 +196,7 @@ public class MenuClientEvents
 					}
 				}
 			}
-			else if (this.configSaveLocation.exists())
-			{
-				try
-				{
-					MenuConfig config = new MenuConfig();
 
-					IOCore.io().readFile(this.configSaveLocation, new NBTFile(this.configSaveLocation, config, MenuConfig.class), new NBTFactory());
-
-					this.openMenu(MenuCore.locate().getMenuFromID(config.menuID), false);
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-			else
-			{
-				this.openMenu(ClientProxy.MINECRAFT_MENU);
-			}
 		}
 	}
 
