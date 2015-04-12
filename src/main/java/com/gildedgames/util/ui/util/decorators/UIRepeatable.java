f682b1bcdd49651a851ba7a34f4571422d0a71b5
Brandon Pearce <brandonpearce@gilded-games.com>
2015-04-12 21:25:40 +0800

    Changed UIRepeatable, it isn't a default decorator

--- src/main/java/com/gildedgames/util/ui/util/decorators/UIRepeatable.java
+++ src/main/java/com/gildedgames/util/ui/util/decorators/UIRepeatable.java
@@ -1,53 +1,69 @@
 package com.gildedgames.util.ui.util.decorators;
 
+import com.gildedgames.util.ui.UIBasicAbstract;
 import com.gildedgames.util.ui.UIView;
 import com.gildedgames.util.ui.data.Dimensions2D;
 import com.gildedgames.util.ui.graphics.Graphics2D;
 import com.gildedgames.util.ui.input.InputProvider;
 
-public class UIRepeatable extends UIScissorable
+public class UIRepeatable extends UIBasicAbstract
 {
+	
+	protected UIScissorable repeatedView;
 
-	public UIRepeatable(UIView view)
+	protected Dimensions2D repeatedArea = new Dimensions2D();
+	
+	public UIRepeatable(UIView repeatedView)
 	{
-		super(view.getDimensions(), view);
+		super(new Dimensions2D());
+		
+		this.repeatedView = new UIScissorable(new Dimensions2D(), repeatedView);
+		
+		this.repeatedView.getDimensions().setOrigin(this);
+	}
+	
+	public Dimensions2D getRepeatedArea()
+	{
+		return this.repeatedArea;
 	}
 
 	@Override
 	public void draw(Graphics2D graphics, InputProvider input)
 	{
-		int textureHeight = this.getDimensions().getHeight();
-		int textureWidth = this.getDimensions().getWidth();
+		this.repeatedView.getScissoredArea().set(this.getDimensions());
+		
+		int textureHeight = this.repeatedView.getDimensions().getHeight();
+		int textureWidth = this.repeatedView.getDimensions().getWidth();
 		
 		int heightCountNeeded = 0;
 		int widthCountNeeded = 0;
 
 		if (textureHeight != 0)
 		{
-			heightCountNeeded = (int) (this.getCutoff().getHeight() / textureHeight);
+			heightCountNeeded = (int) (this.getDimensions().getHeight() / textureHeight);
 		}
 		
 		if (textureWidth != 0)
 		{
-			widthCountNeeded = (int) (this.getCutoff().getWidth() / textureWidth);
+			widthCountNeeded = (int) (this.getDimensions().getWidth() / textureWidth);
 		}
 
-		Dimensions2D oldDim = this.getDimensions().clone();
+		Dimensions2D oldDim = this.repeatedView.getDimensions().clone();
 		
 		for (int heightAmount = 0; heightAmount <= heightCountNeeded; heightAmount++)
 		{
 			for (int widthAmount = 0; widthAmount <= widthCountNeeded; widthAmount++)
 			{
-				super.draw(graphics, input);
+				this.repeatedView.draw(graphics, input);
 				
-				this.getDimensions().addX(textureWidth);
+				this.repeatedView.getDimensions().addX(textureWidth);
 			}
 
-			this.getDimensions().setX(oldDim.getX());
-			this.getDimensions().addY(textureHeight);
+			this.repeatedView.getDimensions().setX(oldDim.getX());
+			this.repeatedView.getDimensions().addY(textureHeight);
 		}
 		
-		this.getDimensions().set(oldDim);
+		this.repeatedView.getDimensions().set(oldDim);
 	}
 	
 }
