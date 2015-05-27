6ddd30b6b24067b3d222a00285e7fa2af6e2cf3a
HEmile <emilevankrieken@live.nl>
2015-05-27 23:20:53 +0200

    Added Collection utilities

--- src/main/java/com/gildedgames/util/collections/CollectionHelper.java
+++ src/main/java/com/gildedgames/util/collections/CollectionHelper.java
@@ -1,7 +1,11 @@
 package com.gildedgames.util.collections;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+
+import com.google.common.base.Function;
 
 public class CollectionHelper
 {
@@ -38,5 +42,32 @@ public class CollectionHelper
 			}
 		}
 		return null;
+	}
+
+	public static int getNextFreeId(Collection<Integer> collection)
+	{
+		return getNextFreeIdFrom(collection, new Function<Integer, Integer>()
+		{
+			@Override
+			public Integer apply(Integer input)
+			{
+				return input;
+			}
+		}, 0);
+	}
+
+	public static <T> int getNextFreeIdFrom(Collection<T> collection, Function<T, Integer> getId, int min)
+	{
+		int expecting = min;
+		final Set<Integer> foundNumbers = new HashSet<Integer>(collection.size());
+		for (final T data : collection)
+		{
+			foundNumbers.add(getId.apply(data));
+			while (foundNumbers.contains(expecting))
+			{
+				expecting++;
+			}
+		}
+		return expecting;
 	}
 }
