194951fa7b7d0291008caab050982a207b523b24
HEmile <emilevankrieken@live.nl>
2015-06-13 15:48:11 +0200

    Added equals and hash to tuple

--- src/main/java/com/gildedgames/util/collections/Tuple.java
+++ src/main/java/com/gildedgames/util/collections/Tuple.java
@@ -21,4 +21,24 @@ public class Tuple<F, S>
 	{
 		return this.second;
 	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Tuple)
+		{
+			Tuple<?, ?> tuple = (Tuple<?, ?>) obj;
+			return tuple.first.equals(this.first) && tuple.second.equals(this.second);
+		}
+		return super.equals(obj);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int hash = 17;
+		hash += this.first.hashCode();
+		hash = hash * 31 + this.second.hashCode();
+		return hash;
+	}
 }
