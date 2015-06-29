ff7b2f1f1135ee4153b2457db068b14327f21d12
devilbringer <devilbringerpayn@gmail.com>
2015-06-29 15:22:01 +0100

    PaynUI Ultimate Edition with Pre-Order DLC Bundle
    Just click the rectangle and watch as your troubles melt away.

--- src/main/java/com/gildedgames/util/core/PaynUI.java
+++ src/main/java/com/gildedgames/util/core/PaynUI.java
@@ -1,11 +1,14 @@
 package com.gildedgames.util.core;
 
 import java.awt.Color;
+import java.util.Random;
 
+import com.gildedgames.util.core.gui.util.MinecraftAssetLocation;
 import com.gildedgames.util.core.gui.util.UIFactory;
 import com.gildedgames.util.ui.common.UIFrame;
 import com.gildedgames.util.ui.data.Dim2D;
 import com.gildedgames.util.ui.data.DrawingData;
+import com.gildedgames.util.ui.graphics.Sprite;
 import com.gildedgames.util.ui.input.InputProvider;
 import com.gildedgames.util.ui.input.MouseButton;
 import com.gildedgames.util.ui.input.MouseInputPool;
@@ -41,6 +44,29 @@ public class PaynUI extends UIFrame
 
 		if (pool.has(MouseButton.LEFT) && input.isHovered(rectangle))
 		{
+            Random random = new Random();
+            
+            int randomX = random.nextInt(input.getScreenWidth());
+            int randomY = random.nextInt(input.getScreenHeight());
+			
+			MinecraftAssetLocation testAsset = new MinecraftAssetLocation(UtilCore.MOD_ID, "textures/someFolder/someTexture.png");
+			 
+			int assetWidth = 310;
+			int assetHeight = 310;
+	 
+			int u = 0;
+			int v = 0;
+	 
+			int textureWidth = 310;
+			int textureHeight = 310;
+	 
+			Sprite testSprite = new Sprite(testAsset, u, v, textureWidth, textureHeight, assetWidth, assetHeight);
+	 
+			UIFrame texture = UIFactory.createTexture(testSprite, Dim2D.build().x(randomX).y(randomY).compile());
+			
+			this.content().setElement("texture" + random.nextInt(50000),texture);
+			
+			texture.modDim().scale(random.nextFloat()).compile();
 			
 		}
 	}
