bcb1864743382e82f586995c62e9c71a42593c9e
Emile van Krieken <emilevankrieken@live.nl>
2015-09-12 20:12:31 +0200

    Added a couple of help methods

--- src/main/java/com/gildedgames/util/GGHelper.java
+++ src/main/java/com/gildedgames/util/GGHelper.java
@@ -7,6 +7,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 
@@ -20,6 +21,11 @@ public class GGHelper
 	public static boolean contains(StructureBoundingBox boundingBox, BlockPos pos)
 	{
 		return pos.getX() >= boundingBox.minX && pos.getX() <= boundingBox.maxX && pos.getZ() >= boundingBox.minZ && pos.getZ() <= boundingBox.maxZ && pos.getY() >= boundingBox.minY && pos.getY() <= boundingBox.maxY;
+	}
+	
+	public static boolean contains(StructureBoundingBox boundingBox, int x, int y, int z)
+	{
+		return x >= boundingBox.minX && x <= boundingBox.maxX && z >= boundingBox.minZ && z <= boundingBox.maxZ && y >= boundingBox.minY && y <= boundingBox.maxY;
 	}
 	
 	/**
@@ -48,5 +54,28 @@ public class GGHelper
 	public static boolean isAir(IBlockState state)
 	{
 		return state.getBlock().getMaterial() == Material.air;
+	}
+	
+	public static BlockPos getBlockPos(NBTTagCompound tag, String key)
+	{
+		final NBTTagCompound newTag = tag.getCompoundTag(key);
+		if (newTag == null || newTag.hasNoTags())
+		{
+			return null;
+		}
+		return new BlockPos(newTag.getInteger("x"), newTag.getInteger("y"), newTag.getInteger("z"));
+	}
+	
+	public static void setBlockPos(NBTTagCompound tag, BlockPos pos, String key)
+	{
+		if (pos == null)
+		{
+			return;
+		}
+		final NBTTagCompound newTag = new NBTTagCompound();
+		newTag.setInteger("x", pos.getX());
+		newTag.setInteger("y", pos.getY());
+		newTag.setInteger("z", pos.getZ());
+		tag.setTag(key, newTag);
 	}
 }
