7faf3cd2a1ea7f16c041ea7c9b4b5d2c5323283d
Emile van Krieken <emilevankrieken@live.nl>
2015-10-22 13:01:05 +0200

    Fixed datainput positions

--- src/main/java/com/gildedgames/util/ui/util/input/GuiInput.java
+++ src/main/java/com/gildedgames/util/ui/util/input/GuiInput.java
@@ -10,7 +10,6 @@ import com.gildedgames.util.core.gui.util.MinecraftAssetLocation;
 import com.gildedgames.util.ui.common.GuiFrame;
 import com.gildedgames.util.ui.data.AssetLocation;
 import com.gildedgames.util.ui.data.Pos2D;
-import com.gildedgames.util.ui.data.rect.Dim2D;
 import com.gildedgames.util.ui.data.rect.Rect;
 import com.gildedgames.util.ui.data.rect.RectModifier.ModifierType;
 import com.gildedgames.util.ui.event.GuiEvent;
@@ -27,24 +26,24 @@ import com.gildedgames.util.ui.util.TextElement;
 
 public class GuiInput extends GuiFrame
 {
-	
+
 	private DataInput data;
-	
+
 	private final AssetLocation texture = new MinecraftAssetLocation(UtilCore.MOD_ID, "textures/gui/test/inputBox.png");
-	
+
 	private boolean isClicked;
-	
+
 	private TextElement input, text;
-	
+
 	private String title;
-	
+
 	private int textIndex;
-	
+
 	public GuiInput(DataInput data, Rect rect, String title)
 	{
 		this.data = data;
 		this.dim().mod().set(rect).flush();
-		
+
 		this.title = title;
 	}
 
@@ -53,47 +52,44 @@ public class GuiInput extends GuiFrame
 	{
 		super.initContent(input);
 
-		GuiFrame inputBox = GuiFactory.createResizableTexture(this.texture, this.dim().clone().clear(ModifierType.POS), UV.build().area(1, 1).flush(), UV.build().area(1, 18).flush(), UV.build().area(198, 1).flush());
-		
 		this.input = new TextElement(GuiFactory.text("", Color.WHITE), Pos2D.flush(0, 0), false);
-		this.text = new TextElement(GuiFactory.text(this.title, Color.WHITE), Pos2D.flush(this.dim().width() / 2, -2), true);
-		
-		GuiFrame test = GuiFactory.createResizableTexture(this.texture, Dim2D.build().area(20, 20).pos(this.dim().width() / 2, -2).flush(), UV.build().area(1, 1).flush(), UV.build().area(1, 18).flush(), UV.build().area(198, 1).flush());
-		
+		this.text = new TextElement(GuiFactory.text(this.title, Color.WHITE), Pos2D.flush(this.dim().width() / 2, 0), true);
+		GuiFrame inputBox = GuiFactory.createResizableTexture(this.texture, this.dim().clone().clear(ModifierType.POS).mod().pos(0, this.text.dim().height()).flush(), UV.build().area(1, 1).flush(), UV.build().area(1, 18).flush(), UV.build().area(198, 1).flush());
+
 		this.input.events().set("onTyping", new GuiEvent()
 		{
-			
+
 			@Override
 			public boolean onKeyboardInput(KeyboardInputPool pool, InputProvider input)
 			{
 				if (GuiInput.this.isClicked && pool.has(ButtonState.PRESSED))
 				{
 					TextElement text = GuiInput.this.input;
-					
+
 					if (pool.has(Keyboard.KEY_DELETE))
 					{
 						text.setText(text.getText().substring(0, text.getText().length() - 2));
 					}
-					
+
 					for (KeyboardInput key : pool)
 					{
-						String keyString = String.valueOf((char)key.getKey());
-						
+						String keyString = String.valueOf((char) key.getKey());
+
 						text.setText(text.getText().concat(keyString));
 					}
 				}
-				
+
 				return super.onKeyboardInput(pool, input);
 			}
 
 			@Override
 			public void initEvent()
 			{
-				
+
 			}
-			
+
 		});
-		
+
 		inputBox.events().set("clicking", new MouseEventGui(new MouseInput(MouseButton.LEFT, ButtonState.PRESSED))
 		{
 
@@ -115,16 +111,17 @@ public class GuiInput extends GuiFrame
 			@Override
 			public void initEvent()
 			{
-				
+
 			}
-	
+
 		});
-		
+
 		this.content().set("inputBox", inputBox);
 
-		this.content().set("test", test);
 		this.content().set("text", this.text);
 		this.content().set("input", this.input);
+
+		this.dim().mod().addHeight(this.text.dim().height()).flush();
 	}
-	
+
 }
