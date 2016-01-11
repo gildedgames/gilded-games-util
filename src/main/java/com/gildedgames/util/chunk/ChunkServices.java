fde168c5b80bd398e8d29a640f51ab66c8dcaad5
Collin1971 <collin785@gmail.com>
2016-01-10 20:09:54 -0600

    Added chunk hooks

---
+++ src/main/java/com/gildedgames/util/chunk/ChunkServices.java
@@ -0,0 +1,151 @@
+package com.gildedgames.util.chunk;
+
+import com.gildedgames.util.chunk.common.hook.IChunkHook;
+import com.gildedgames.util.chunk.common.hook.IChunkHookFactory;
+import com.gildedgames.util.chunk.common.pools.IChunkHookPool;
+import com.gildedgames.util.chunk.common.pools.WorldHookPool;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.event.world.ChunkDataEvent;
+import net.minecraftforge.event.world.ChunkEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChunkServices
+{
+	private final Map<Integer, WorldHookPool> pools = new HashMap<>();
+
+	private final List<IChunkHookFactory<IChunkHook>> factories = new ArrayList<>();
+
+	@SubscribeEvent
+	public void onChunkLoaded(ChunkDataEvent.Load event)
+	{
+		WorldHookPool worldPool = this.getWorldPool(event.world);
+
+		if (worldPool == null)
+		{
+			// Create our chunk pool for the dimension if we haven't already
+			worldPool = this.createWorldPool(event.world);
+		}
+
+		long coord = this.getChunkCoord(event.getChunk());
+
+		// Build each hook using our registered factories for this chunk
+		for (IChunkHookFactory<IChunkHook> factory : this.factories)
+		{
+			IChunkHook hook = factory.createHook(event.world, event.getData());
+
+			IChunkHookPool hookPool = worldPool.getPool(hook.getClass());
+
+			if (hookPool == null)
+			{
+				hookPool = worldPool.addPool(hook.getClass(), factory.createPool());
+			}
+
+			hookPool.addChunkHook(hook, coord);
+		}
+	}
+
+	@SubscribeEvent
+	public void onChunkUnloaded(ChunkEvent.Unload event)
+	{
+		WorldHookPool worldPool = this.getWorldPool(event.world);
+
+		if (worldPool != null)
+		{
+			for (IChunkHookPool hookPool : worldPool.getAllPools())
+			{
+				hookPool.removeChunkHook(this.getChunkCoord(event.getChunk()));
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onChunkSaved(ChunkDataEvent.Save event)
+	{
+		WorldHookPool worldPool = this.getWorldPool(event.world);
+
+		if (worldPool != null)
+		{
+			for (IChunkHookPool hookPool : worldPool.getAllPools())
+			{
+				hookPool.saveChunkHook(event.getData(), this.getChunkCoord(event.getChunk()));
+			}
+		}
+	}
+
+	/**
+	 * Registers a chunk hook factory. The factory is responsible for the creation of it's own chunk hooks from NBT.
+	 * @param factory The factory to register.
+	 */
+	public void registerHookFactory(IChunkHookFactory<IChunkHook> factory)
+	{
+		if (this.factories.contains(factory))
+		{
+			throw new IllegalArgumentException("Factory " + factory.getClass().getCanonicalName() + " is already registered!");
+		}
+
+		this.factories.add(factory);
+	}
+
+	private WorldHookPool getWorldPool(World world)
+	{
+		return this.pools.get(world.provider.getDimensionId());
+	}
+
+	/**
+	 * Creates an empty pool for a dimension.
+	 * @param world The dimension/world.
+	 * @return A new empty pool.
+	 */
+	private WorldHookPool createWorldPool(World world)
+	{
+		WorldHookPool pool = new WorldHookPool();
+
+		this.pools.put(world.provider.getDimensionId(), pool);
+
+		return pool;
+	}
+
+	/**
+	 * @param chunk The chunk
+	 * @return Returns a unique identifier that represents the chunk's position.
+	 */
+	private long getChunkCoord(Chunk chunk)
+	{
+		return ChunkCoordIntPair.chunkXZ2Int(chunk.xPosition, chunk.zPosition);
+	}
+
+	public <T extends IChunkHook> IChunkHook getData(World world, BlockPos pos, Class<T> clazz)
+	{
+		WorldHookPool pool = this.getWorldPool(world);
+
+		if (pool != null)
+		{
+			int x = pos.getX() / 16, z = pos.getZ() / 16;
+
+			IChunkHookPool hookPool = pool.getPool(clazz);
+
+			if (hookPool != null)
+			{
+				return hookPool.getHook(ChunkCoordIntPair.chunkXZ2Int(x, z));
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Destroys all currently loaded pools.
+	 */
+	public void clean()
+	{
+		this.pools.clear();
+	}
+}
---
+++ src/main/java/com/gildedgames/util/chunk/common/hook/IChunkHookFactory.java
@@ -0,0 +1,22 @@
+package com.gildedgames.util.chunk.common.hook;
+
+import com.gildedgames.util.chunk.common.pools.IChunkHookPool;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public interface IChunkHookFactory<T extends IChunkHook>
+{
+	/**
+	 * Creates a hook which links into a regular chunk.
+	 * @param world The chunk's world
+	 * @param tag The chunk's NBT
+	 * @return A new hook {@link T}
+	 */
+	T createHook(World world, NBTTagCompound tag);
+
+	/**
+	 * Creates a pool which stores this factory's hooks.
+	 * @return An empty {@link IChunkHookPool <T>}
+	 */
+	IChunkHookPool createPool();
+}
