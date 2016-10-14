07c27b506658363f8c5667ff3d93ef5f364a522c
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-10-15 05:36:46 +0800

    Implemented new constructor for SpriteGeneric

--- src/main/java/com/gildedgames/util/core/client/SpriteGeneric.java
+++ src/main/java/com/gildedgames/util/core/client/SpriteGeneric.java
@@ -5,6 +5,11 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 public class SpriteGeneric extends TextureAtlasSprite
 {
 
+	public SpriteGeneric(String iconName)
+	{
+		super(iconName);
+	}
+
 	public SpriteGeneric(String iconName, int width, int height)
 	{
 		super(iconName);
